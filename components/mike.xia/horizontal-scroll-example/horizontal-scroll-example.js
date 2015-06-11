3167e9f512ec29809d3c503b3dccc67e24217e14
Michael Xia <xia.umd@gmail.com>
2015-06-11 01:12:46 -0700

    added example for scroll view

---
+++ components/mike.xia/horizontal-scroll-example/horizontal-scroll-example.js
@@ -0,0 +1,46 @@
+BEST.scene('mike.xia:horizontal-scroll-example', {
+    behaviors: {
+        '$self': {
+        },
+        '.scroll-view' : {
+            'direction' : 'vertical',
+            'item-sizes' : function(tweetCount, tweetSize) {
+                var sizes = [];
+                for(var i=0; i<tweetCount; i++) {
+                    sizes.push(tweetSize);
+                }
+                return sizes;
+            }
+        },
+        '.scroll-view-item' : {
+            style: {
+                'border' : '1px solid black',
+                'backgroundColor' : 'white',
+                'font-family' : 'Lato',
+                'font-size' : '30px'
+            },
+            '$repeat' : function(tweetCount, tweetSize) {
+                var stuff = []; 
+                for(var i=0; i<tweetCount; i++) {
+                    stuff.push({
+                        content : 'Item' + i,
+                        size : tweetSize
+                    });
+                }
+                return stuff;
+            }
+        }
+    },
+    events: {
+    },
+    states: {
+        tweetCount : 50,
+        tweetSize : [400, 100]
+    }, 
+    tree: 'horizontal-scroll-example.jade'
+})
+.config({
+    imports: {
+        'mike.xia' : ['scroll-view']
+    }
+});
