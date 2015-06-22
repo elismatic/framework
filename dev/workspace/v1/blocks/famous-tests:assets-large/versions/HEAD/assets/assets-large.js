3b2d1dbfb8523e9412743e2d32819f225a390b0f
Matthew Trost <matthew@famo.us>
2015-06-22 11:00:06 -0700

    chore: Remove 'dev' folder files I didn't mean to commit

--- dev/workspace/v1/blocks/famous-tests:assets-large/versions/HEAD/assets/assets-large.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-FamousFramework.module('famous-tests:assets-large', {
-    tree: '\n        <h1 style="color:white;">You should see a large image of a galaxy here</h1>\n        <img src="{{@galaxy.jpg}}">\n    '
-});
--- dev/workspace/v1/blocks/famous-tests:misspelled-facets/versions/HEAD/assets/misspelled-facets.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-FamousFramework.scene('famous-tests:misspelled-facets', {
-    behaviours: {},
-    state: {},
-    event: {},
-    template: '<node></node>',
-    tree: '\n        <h1 style="color:white;">You should see console warnings about properties that don\'t belong in the component definition</h1>\n    '
-});
